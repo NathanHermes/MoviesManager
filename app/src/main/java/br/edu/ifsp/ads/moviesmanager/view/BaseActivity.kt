edc2a39eb006e51c7bdef16d2a71dacb54d490dc
NathanHermes <hermesnathan8@gmail.com>
2023-06-25 16:12:08 -0300

    :sparkles: Feat: Create global extras

---
+++ app/src/main/java/br/edu/ifsp/ads/moviesmanager/view/BaseActivity.kt
@@ -0,0 +1,8 @@
+package br.edu.ifsp.ads.moviesmanager.view
+
+import androidx.appcompat.app.AppCompatActivity
+
+class BaseActivity : AppCompatActivity() {
+  protected val EXTRA_MOVIE = "Movie"
+  protected val EXTRA_VIEW_MOVIE = "ViewMovie"
+}
