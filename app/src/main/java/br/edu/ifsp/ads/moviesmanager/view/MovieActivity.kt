65823d7a5daefcfcec4ff7490b86a026c7590d0a
NathanHermes <hermesnathan8@gmail.com>
2023-06-25 16:44:47 -0300

    :sparkles: Feat: Create movie activity

---
+++ app/src/main/java/br/edu/ifsp/ads/moviesmanager/view/MovieActivity.kt
@@ -0,0 +1,12 @@
+package br.edu.ifsp.ads.moviesmanager.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.edu.ifsp.ads.moviesmanager.R
+
+class MovieActivity : AppCompatActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_movie)
+  }
+}
