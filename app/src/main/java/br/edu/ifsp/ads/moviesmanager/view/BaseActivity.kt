43ee21bc7677c729e8362fdf7a86f842b877e4bc
NathanHermes <hermesnathan8@gmail.com>
2023-06-25 16:28:36 -0300

    :bug: Fix: Define class to sealed

--- app/src/main/java/br/edu/ifsp/ads/moviesmanager/view/BaseActivity.kt
+++ app/src/main/java/br/edu/ifsp/ads/moviesmanager/view/BaseActivity.kt
@@ -2,7 +2,8 @@ package br.edu.ifsp.ads.moviesmanager.view
 
 import androidx.appcompat.app.AppCompatActivity
 
-class BaseActivity : AppCompatActivity() {
+sealed class BaseActivity : AppCompatActivity() {
   protected val EXTRA_MOVIE = "Movie"
+  protected val EXTRA_MOVIES_LIST = "MoviesList"
   protected val EXTRA_VIEW_MOVIE = "ViewMovie"
 }
